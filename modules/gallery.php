4b79a66a1090659519c5aae307b333e045a69693
Jaydeep Dharamsey <33755235+Jaydeep21@users.noreply.github.com>
2018-08-16 23:10:36 +0530

    Add files via upload

--- modules/gallery.php
+++ modules/gallery.php
@@ -25,11 +25,90 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Karma", sans-serif}
 header{
   z-index: 9999;
 }
-
+img:hover{
+    box-shadow: 8px 8px 5px 0 rgba(0,0,0,.2);
+}
 .w3-quarter img {
     height: 180px;
     width: 100%;
     padding: 5px;
+    
+}
+.modal {
+    display: none; /* Hidden by default */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    padding-top: 100px; /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+}
+
+/* Modal Content (image) */
+.modal-content {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+}
+
+/* Caption of Modal Image */
+#caption {
+    margin: auto;
+    display: block;
+    width: 80%;
+    max-width: 700px;
+    text-align: center;
+    color: #ccc;
+    padding: 10px 0;
+    height: 150px;
+}
+
+/* Add Animation */
+.modal-content, #caption {    
+    -webkit-animation-name: zoom;
+    -webkit-animation-duration: 0.6s;
+    animation-name: zoom;
+    animation-duration: 0.6s;
+}
+
+@-webkit-keyframes zoom {
+    from {-webkit-transform:scale(0)} 
+    to {-webkit-transform:scale(1)}
+}
+
+@keyframes zoom {
+    from {transform:scale(0)} 
+    to {transform:scale(1)}
+}
+
+/* The Close Button */
+.close {
+    position: absolute;
+    top: 100px;
+    right: 35px;
+    color: #f1f1f1;
+    font-size: 40px;
+    font-weight: bold;
+    transition: 0.3s;
+}
+
+.close:hover,
+.close:focus {
+    color: #bbb;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+/* 100% Image Width on Smaller Screens */
+@media only screen and (max-width: 700px){
+    .modal-content {
+        width: 100%;
+    }
 }
 </style>
 </head>
@@ -42,19 +121,19 @@ header{
   <!-- First Photo Grid-->
   <div class="w3-row-padding w3-padding-16 w3-center" id="food">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or1.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or2.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or3.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or4.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
   </div>
@@ -62,19 +141,19 @@ header{
   <!-- Second Photo Grid-->
   <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or5.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or5.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or6.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or6.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or7.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or7.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/or8.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/or8.JPG" alt="SAKEC ACM Student Chapter Formation" style="width:100%" onclick="return myFunction(this)">
       
     </div>
    
@@ -84,19 +163,19 @@ header{
  <!-- Third Photo Grid-->
   <div class="w3-row-padding w3-padding-16 w3-center" id="food">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/grp1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/grp1.JPG" alt="ACM Members" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/grp2.JPG" aalt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/grp2.JPG" alt="ACM Members" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/grp3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/grp3.JPG" alt="ACM Members" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/grp4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/grp4.JPG" alt="ACM Members" style="width:100%" onclick="return myFunction(this)">
       
     </div>
    
@@ -107,19 +186,19 @@ header{
   <!-- Fourth Photo Grid-->
   <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/py1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/py1.JPG" alt="Python" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/py2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/py2.JPG" alt="Python" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/py3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/py3.JPG" alt="Python" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/py4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/py4.JPG" alt="Python" style="width:100%" onclick="return myFunction(this)">
       
     </div>
    
@@ -129,19 +208,19 @@ header{
 <h3 align="center">Cyber H4ck</h3>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/cy1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/cy1.JPG" alt="Cyber H4ck" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/cy2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/cy2.JPG" alt="Cyber H4ck" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/cy3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/cy3.JPG" alt="Cyber H4ck" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/cy4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/cy4.JPG" alt="Cyber H4ck" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
@@ -150,19 +229,19 @@ header{
 <h3 align="center">Android</h3>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/an1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/an1.JPG" alt="Android" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/an2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/an2.JPG" alt="Android" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/an3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/an3.JPG" alt="Android" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/an4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/an4.JPG" alt="Android" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
@@ -172,19 +251,19 @@ header{
 <h3 align="center">PC Assembly</h3>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pc1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pc1.JPG" alt="PC Assembly" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pc2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pc2.JPG" alt="PC Assembly" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pc3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pc3.JPG" alt="PC Assembly" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pc4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pc4.JPG" alt="PC Assembly" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
@@ -193,37 +272,37 @@ header{
 <h3 align="center">Trek</h3>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk1.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk2.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk3.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk4.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk5.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk5.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk6.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk6.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk7.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk7.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/trk8.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/trk8.JPG" alt="Trek" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
@@ -233,37 +312,37 @@ header{
 <h3 align="center">IoT</h3>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot1.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot2.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot2.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot3.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot4.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
 <div class="w3-row-padding w3-padding-16 w3-center">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot5.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot5.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot6.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot6.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot7.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot7.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/iot8.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/iot8.JPG" alt="IoT" style="width:100%" onclick="return myFunction(this)">
       
     </div>
 </div>
@@ -272,19 +351,19 @@ header{
  <!-- Third Photo Grid-->
   <div class="w3-row-padding w3-padding-16 w3-center" id="food">
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pcra1.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pcra1.JPG" alt="PCRA" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pcra2.JPG" aalt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pcra2.JPG" aalt="PCRA" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pcra3.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pcra3.JPG" alt="PCRA" style="width:100%" onclick="return myFunction(this)">
       
     </div>
     <div class="w3-quarter">
-      <img src="../assets/img/gallery/pcra4.JPG" alt="sakec acm" style="width:100%">
+      <img src="../assets/img/gallery/pcra4.JPG" alt="PCRA" style="width:100%" onclick="return myFunction(this)">
       
     </div>
    
@@ -298,7 +377,11 @@ header{
 
 
  
-
+<div id="myModal" class="modal">
+  <span class="close">&times;</span>
+  <img class="modal-content" id="img01">
+  <div id="caption"></div>
+</div>
 <!-- End page content -->
 </div>
 
@@ -326,6 +409,26 @@ function w3_open() {
  
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
+}
+// Get the modal
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var captionText = document.getElementById("caption");
+//entering image into modal
+function myFunction(img){
+    var imag=img.src;
+    var alter=img.alt;
+    modal.style.display = "block";
+    modalImg.src =imag;
+    captionText.innerHTML = alter;
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
 }
 </script>
 
